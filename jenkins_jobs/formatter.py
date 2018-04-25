7f4d4fed4cfb175d9d24b455f7f19baf673b7add
Darragh Bailey <dbailey@hpe.com>
2018-04-25 15:44:16 +0100

    Allow dashes in variable key names
    It can be confusing when adding variables to templates and macros to
    discover that '-' are not valid characters in the key name.
    
    (cherry picked from commit b92336aa95c7b06316ece9e0537fb3d7c150279c)
    
    Change-Id: I8b10371ddac54c19a92a9d0811cd549a492b3dbb
    Signed-off-by: Tomas Janousek <tomas.janousek@gooddata.com>
    Signed-off-by: Jan Hruban <jan.hruban@gooddata.com>
    JIRA: SETI-1326

--- jenkins_jobs/formatter.py
+++ jenkins_jobs/formatter.py
@@ -86,7 +86,7 @@ class CustomFormatter(Formatter):
     _expr = """
         (?<!{){({{)*                # non-pair opening {
         (?:obj:)?                   # obj:
-        (?P<key>\w+)                # key
+        (?P<key>[\w\-]+)            # key
         (?:\|(?P<default>[^}]*))?   # default fallback
         }(}})*(?!})                 # non-pair closing }
     """
